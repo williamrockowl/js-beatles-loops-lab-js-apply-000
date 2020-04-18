b8aa4846e60eb6fd41599571934549f21fa96b1a
William Saulon <williamsaulon24@gmail.com>
2020-04-18 04:49:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // add solution here
+` const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice" ];`
 function theBeatlesPlay(musicians, instruments){
 var ans = [];
 for(let i = 0;i < musicians.length; i++){
@@ -11,11 +16,7 @@ return ans;
 function johnLennonFacts(facts){
   
   const answer = [];
- ` const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice" ];`
+ 
   let i = 0;
   while(i < facts.lenth){
    answer.push(`${facts[i]}!!!`);
