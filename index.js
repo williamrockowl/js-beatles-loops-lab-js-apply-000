6c6d661007ac0a632a159bd6f6086d1f23566c6b
William Saulon <williamsaulon24@gmail.com>
2020-04-18 04:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+var ans = [];
+for(let i = 0;i < musicians.length; i++){
+  ans = [musicians[0] + " plays " + instruments[0], musicians[1] + " plays " + instruments[1],musicians[2] + " plays " + instruments[2],
+  musicians[3] + " plays " + instruments[3]];
+  return ans;
+}
+function johnLennonFacts(facts1){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice" ];
+  while(i < facts.lenth){
+  facts1 += facts[i] + "!!!";
+  i++;
+  }
+  facts1 = facts;
+  return facts1;   
+  
+  }
+  function iLoveTheBeatles(number){
+    var emp = [];
+    do{
+      
+    }
+    while(<15i);
+    
+  }
+  
+
+ 
+  
+}
